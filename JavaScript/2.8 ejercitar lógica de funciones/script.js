266d15316f53541175fe6fd1684131ca7774c775
Marcell Figueroa <mfig9971@gmail.com>
2025-07-29 14:03:08 -0400

    .

---
+++ JavaScript/2.8 ejercitar lógica de funciones/script.js
@@ -0,0 +1,22 @@
+function secuencia(numeros){
+    let resultado = []
+    for (let i = 1; i <= numeros; i++){
+        if (i % 4 === 0){
+            resultado.push(`${i}*`)
+        }
+        else{
+            resultado.push(i)
+        }
+    }
+    return resultado.join(", ")
+}
+
+function mostrarSecuencia(){
+    let input1 = document.getElementById("input1");
+    let numeros = parseFloat(input1.value);
+    if (isNaN(numeros)){
+        alert("Ingrese solo números enteros!!.")
+    }
+    let resultadoFinal = secuencia(numeros)
+    document.getElementById("resultado").textContent = resultadoFinal;
+}
---
+++ JavaScript/Evaluación 29-07-2025/script.js
@@ -0,0 +1,32 @@
+// Ejercicio 2: Creación de función con parámetros y botón
+
+// Crea un programa en JavaScript que cumpla los siguientes requisitos:
+// Debe existir un input de texto en HTML donde el usuario ingrese un número.
+// Un botón debe ejecutar una función que reciba como parámetro el número ingresado y retorne todos los números impares desde 1 hasta el número dado.
+// El resultado debe mostrarse con alert y en la consola usando console.log.
+// Usa for, if y lógica con return para generar los valores.
+// Pública el proyecto en GitHub y adjunta una captura de pantalla de la ejecución.
+
+// Ejemplo esperado:
+// Si el usuario ingresa 10, el resultado debe ser:
+// 1, 3, 5, 7, 9
+
+
+function numeroImpar(numero){
+    let resultado = [];
+    for(let i = 0; i <= numero; i++){
+        if (i % 2 !== 0) {
+            resultado.push(i)
+        }
+    }
+    return resultado;
+}
+function mostrarImpar() {
+    let input = document.getElementById("input1");
+    let numero = parseInt(input.value);
+    let resultadoFinal = numeroImpar(numero);
+    document.getElementById("resultado").textContent = resultadoFinal;
+    if (isNaN(numero)) {
+        alert("Ingrese un número válido.");
+    }
+}
