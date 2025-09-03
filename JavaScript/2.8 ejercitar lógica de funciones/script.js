f94bdd228b4010d6255c2d867ec64ff97c310512
Marcell <marcell25208@gmail.com>
2025-09-02 22:24:53 -0400

    .

--- JavaScript/2.8 ejercitar lógica de funciones/script.js
+++ JavaScript/2.8 ejercitar lógica de funciones/script.js
@@ -1,3 +1,13 @@
+// Ejercicio 2: Programa con Función y Botón
+// Instrucción:
+//  Crea un programa en JavaScript que cumpla con lo siguiente:
+// Debe existir un input de texto en HTML donde el usuario ingrese un número.
+// Un botón debe ejecutar una función llamada mostrarSecuencia que reciba como parámetro el número ingresado.
+// La función debe generar y retornar una cadena con todos los números desde 1 hasta el número 
+// ingresado, marcando con un asterisco (*) aquellos que sean múltiplos de 4.
+// El resultado debe mostrarse en el html.
+// Publicar el proyecto en GitHub y adjuntar una captura de pantalla de la ejecución.
+
 function secuencia(numeros){
     let resultado = []
     for (let i = 1; i <= numeros; i++){
